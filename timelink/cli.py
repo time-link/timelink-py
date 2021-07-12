d938a6410b6937db282eb02123e4b5217c5d3ef2
joaquimrcarvalho <joaquim@uc.pt>
2021-07-12 22:42:43 +0800

    Reproduces mhk manager version information

--- timelink/cli.py
+++ timelink/cli.py
@@ -4,9 +4,11 @@ Also provides basic mhk manager functionality.
 
 
 """
+import platform
 import click
-from timelink.mhk.utilities import get_mhk_env
+from timelink.mhk.utilities import get_mhk_env, get_mhk_app_env
 import docker
+
 
 @click.group()
 def cli():
@@ -23,14 +25,38 @@ def mhk():
 @mhk.command(name='version')
 def mhk_version():
     """shows MHK manager version"""
+
+    # this should go to mhk.utilities as get_mhk_info()
     mhk_env = get_mhk_env()
+    user_home = mhk_env['HOST_MHK_USER_HOME']
     mhk_home = mhk_env['HOST_MHK_HOME']
     with open(mhk_home + '/app/manager_version', 'r') as file:
-        mv = file.readlines()
-    click.echo(mv[0].replace('\n',''))
+        mv = file.read().replace('\n', '')
+    with open(mhk_home + '/.mhk-home', 'r') as file:
+        mhk_home_update = file.read().replace('\n', '')
+    with open(mhk_home + '/.mhk-home-manager-init', 'r') as file:
+        mhk_home_init = file.read().replace('\n', '')
+
+    mhk_app_env = get_mhk_app_env()
+    mhk_host = mhk_app_env.get('MHK_HOST', 'localhost')
+    # end of get_hmk_info
+
     client = docker.from_env()
-    docker_version = client.version()
-    print(docker_version['Platform']['Name'],docker_version['Version'])
+    dv = client.version()
+    mhkv = \
+        f"""    Manager version:  {mv}
+    Docker version:   {dv["Version"]}
+    Host OS:          {platform.system()} {platform.release()}
+    User home:        {user_home}
+    mhk-home:         {mhk_home}
+    mhk-home init:    {mhk_home_init}
+    mhk-home update:  {mhk_home_update}
+    mhk use-tag:      {mhk_app_env.get("TAG", "*none*")}
+    mhk local host:   {mhk_host}
+    MHK URL:          http://127.0.0.1:8080/mhk
+    Kleio URL:        http://127.0.0.1:8088
+    Portainer URL:    http://127.0.0.1:9000"""
+    print(mhkv)
     return 0
 
 
@@ -45,6 +71,7 @@ def mhk_status():
        Stopped  :{dinfo['ContainersStopped']}
     """)
     return 0
+
 
 @click.command()
 def timelink():
