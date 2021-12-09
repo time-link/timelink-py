8559d442a67a1fbd1d364f3dd08bad2156944df5
Joaquim Carvalho <joaquim@uc.pt>
2021-12-09 15:38:14 +0800

    Add import directly from github files

--- tests/test_import.py
+++ tests/test_import.py
@@ -59,4 +59,17 @@ def test_import_with_many(dbsystem):
         stats = import_from_xml(file, session, options={'return_stats': True})
         estudante = session.get(Entity, '140771')
         kleio = estudante.to_kleio()
-        assert estudante is not None, "could not get an entity from big import"
+        assert estudante is not None, "could not get an entity from big import"@skip_on_travis
+
+
+@skip_on_travis
+def test_import_git_hub(dbsystem):
+    file = "https://raw.githubusercontent.com/time-link/timelink-py/f76007cb7b98b39b22be8b70b3b2a62e7ae0c12f/tests/xml_data/b1685.xml"
+    with Session() as session:
+
+        stats = import_from_xml(file, session, options={'return_stats': True})
+
+        domingos_vargas = session.get(Person, 'b1685.33-per6')
+        assert domingos_vargas is not None, "could not get a person from file"
+        kleio = domingos_vargas.to_kleio()
+        pass
