d030bb24e64784bfbe84e3ddfc7a81d2d365ee15
Joaquim Carvalho <joaquim@uc.pt>
2021-12-10 19:16:42 +0800

    Improve __str__ quote as in Kleio.

--- timelink/mhk/models/source.py
+++ timelink/mhk/models/source.py
@@ -5,6 +5,7 @@ MIT License, no warranties.
 from sqlalchemy import Column, String, ForeignKey
 
 from timelink.mhk.models.base import Entity
+from timelink.kleio.utilities import kleio_escape, quote_long_text
 
 class Source(Entity):
     __tablename__ = 'sources'
@@ -32,13 +33,14 @@ class Source(Entity):
             f'ref="{self.ref}", '
             f'kleiofile="{self.kleiofile}", '
             f'replaces="{self.replaces}", '
-            f'obs={self.obs}'
+            f'obs="{self.obs}"'
             f')'
         )
 
     def __str__(self):
-        r = f'{self.groupname}${self.id}/{self.the_date}/type={self.the_type}/ref={self.ref}/loc={self.loc}/kleiofile={self.kleiofile}/replaces={self.replaces}'
+        r = f'{self.groupname}${self.id}/{self.the_date}/type={kleio_escape(self.the_type)}/ref={kleio_escape(self.ref)}/loc={kleio_escape(self.loc)}/kleiofile={kleio_escape(self.kleiofile)}"' \
+            f'/replaces={self.replaces}'
         if self.obs is not None:
-                r = (f'{r}  /obs={self.obs}')
+                r = (f'{r}  /obs={quote_long_text(self.obs)}')
         return r
 
