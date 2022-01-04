d65d2d26a1aa85ffc9d6e4f979c2bdd0e6fc1450
joaquimrcarvalho <joaquim@uc.pt>
2022-01-04 16:52:09 +0000

    Improve usage of MHK utilities module

--- timelink/cli.py
+++ timelink/cli.py
@@ -2,82 +2,71 @@
 
 Also provides basic mhk manager functionality.
 
-TODO consider typer https://typer.tiangolo.com
-
 """
 import typer
 import platform
-from timelink.mhk.utilities import get_mhk_env, get_mhk_app_env
 import docker
+from timelink.mhk.utilities import MHKInfo, get_mhk_info, is_mhk_installed
 
 # We use Typer https://typer.tiangolo.com
-
 app = typer.Typer(help="Timelink and MHK manager")
 mhk_app = typer.Typer()
-app.add_typer(mhk_app,name="mhk", help="MHK legacy manager")
+app.add_typer(mhk_app, name="mhk", help="MHK legacy manager")
 
-@mhk_app.command(name='version')
+
+@mhk_app.command(name="version")
 def mhk_version():
     """shows MHK manager version
 
     Demonstrates how to access MHK installation files and
     usage of Docker API
     """
+    if is_mhk_installed():
+        mhk_info: MHKInfo = get_mhk_info()
 
-    # this should go to mhk.utilities as get_mhk_info()
-    mhk_env = get_mhk_env()
-    user_home = mhk_env['HOST_MHK_USER_HOME']
-    mhk_home = mhk_env['HOST_MHK_HOME']
-    with open(mhk_home + '/app/manager_version', 'r') as file:
-        mv = file.read().replace('\n', '')
-    with open(mhk_home + '/.mhk-home', 'r') as file:
-        mhk_home_update = file.read().replace('\n', '')
-    with open(mhk_home + '/.mhk-home-manager-init', 'r') as file:
-        mhk_home_init = file.read().replace('\n', '')
-
-    mhk_app_env = get_mhk_app_env()
-    mhk_host = mhk_app_env.get('MHK_HOST', 'localhost')
-    # end of get_hmk_info
-
-    client = docker.from_env()
-    dv = client.version()
-    mhkv = \
-        f"""    Manager version:  {mv}
-    Docker version:   {dv["Version"]}
-    Host OS:          {platform.system()} {platform.release()}
-    User home:        {user_home}
-    mhk-home:         {mhk_home}
-    mhk-home init:    {mhk_home_init}
-    mhk-home update:  {mhk_home_update}
-    mhk use-tag:      {mhk_app_env.get("TAG", "*none*")}
-    mhk local host:   {mhk_host}
-    MHK URL:          http://127.0.0.1:8080/mhk
-    Kleio URL:        http://127.0.0.1:8088
-    Portainer URL:    http://127.0.0.1:9000"""
-    typer.echo(mhkv)
+        client = docker.from_env()
+        dv = client.version()
+        mhkv = f"""    Manager version:  {mhk_info['mhk_version']}
+        Docker version:   {dv["Version"]}
+        Host OS:          {platform.system()} {platform.release()}
+        User home:        {mhk_info['user_home']}
+        mhk-home:         {mhk_info['mhk_home']}
+        mhk-home init:    {mhk_info['mhk_home_init']}
+        mhk-home update:  {mhk_info['mhk_home_update']}
+        mhk use-tag:      {mhk_info['mhk_app_env'].get("TAG", "*none*")}
+        mhk local host:   {mhk_info['mhk_host']}
+        MHK URL:          http://127.0.0.1:8080/mhk
+        Kleio URL:        http://127.0.0.1:8088
+        Portainer URL:    http://127.0.0.1:9000"""
+        typer.echo(mhkv)
+    else:
+        type.echo("Could not find a MHK instalation")
     return 0
 
 
-@mhk_app.command(name='status')
+@mhk_app.command(name="status")
 def mhk_status():
     """shows docker status information"""
     client = docker.from_env()
     dinfo = client.info()
-    typer.echo(f"""
+    typer.echo(
+        f"""
     Containers  :{dinfo['Containers']}
        Running  :{dinfo['ContainersRunning']}
        Paused   :{dinfo['ContainersPaused']}
        Stopped  :{dinfo['ContainersStopped']}
-    """)
+    """
+    )
     return 0
+
 
 @app.callback()
 def main():
     """
-     This is the timelink/MHK manager on the command line
-     """
+    This is the timelink/MHK manager on the command line
+    """
 
-    typer.echo("This is the timelink/MHK manager on the command line")
+    typer.echo("This is the timelink/MHK manager")
 
 
 if __name__ == "__main__":
