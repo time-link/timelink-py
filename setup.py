e1a5f272537f79b6d2a8a2c7a87b25b2f6826a52
joaquimrcarvalho <joaquim@uc.pt>
2022-06-07 22:30:12 +0800

    Bump version: 0.3.9 → 0.3.10

--- setup.py
+++ setup.py
@@ -49,6 +49,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/time-link/timelink-py',
-    version='0.3.9',
+    version='0.3.10',
     zip_safe=False,
 )
