f3891708cc1c3d77fb26ce413600618ffb48dcd5
Joaquim Carvalho <joaquim@uc.pt>
2024-01-29 18:40:47 +0800

    Update python versions

--- setup.py
+++ setup.py
@@ -25,9 +25,9 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
     ],
     description=description,
     entry_points={
