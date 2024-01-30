98249fbc1a092db932833b78699bef287f970721
Joaquim Carvalho <joaquim@uc.pt>
2024-01-30 21:32:53 +0800

    Bump version: 1.0.0 → 1.0.1

--- setup.py
+++ setup.py
@@ -63,6 +63,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/time-link/timelink-py',
-    version='1.0.0',
+    version='1.0.1',
     zip_safe=False,
 )
