eb5bc178a847e09bc66e9db5e966f4b441e63f1b
Joaquim Carvalho <joaquim@uc.pt>
2024-02-06 17:19:05 +0800

    Refactores for anytype of entity

--- timelink/pandas/entities_with_attribute.py
+++ timelink/pandas/entities_with_attribute.py
@@ -8,7 +8,8 @@ def entities_with_attribute(
     the_type,
     the_value=None,
     column_name=None,
-    person_info=True,
+    entity_type="entity",
+    more_info=None,
     dates_in=None,
     name_like=None,
     filter_by=None,
@@ -20,10 +21,11 @@ def entities_with_attribute(
 
     Args:
         the_type    : type of attribute, can have SQL wildcards, string
+        the_value   : if present, limit to this value, can be SQL wildcard,
+        entity_type : if present, limit to this entity type, string
         column_name : if present, use this name for the column, otherwise use the_type
         the_value   : if present, limit to this value, can be SQL wildcard,
-                       string or list of strings
-        person_info : if True add name and sex of person, otherwise just id
+        more_info   : List of extra columns from entity_type to add to the dataframe
         dates_in    : (after,before) if present only between those dates (exclusive)
         name_like   : name must match pattern (will set person_info = True),
         filter_by   : list of ids, limit to these
@@ -51,39 +53,53 @@ def entities_with_attribute(
             )
         )
 
-        # if we dont have a name for the column we use the attribute type sanitized
+    # if we dont have a name for the column we use the attribute type sanitized
     if column_name is None:
         column_name = the_type
     date_column_name = f"{column_name}.date"
     obs_column_name = f"{column_name}.obs"
 
-    if name_like is not None:
-        if person_info is not None and not person_info:
-            raise (ValueError("To filter by name requires person_info=True."))
-        person_info = True
+    entity_types = db.get_models_ids()
+    if entity_type not in entity_types:
+        raise ValueError(f"entity_type must be one of {entity_types}")
 
-    if person_info:  # to fetch person info we need nattributes view
-        attr = db.get_nattribute_view()
-        id_col = attr.c.id
-        stmt = select(
-            attr.c.id,
-            attr.c.name.label("name"),
-            attr.c.sex.label("sex"),
-            attr.c.the_value.label(column_name),
-            attr.c.the_date.label(date_column_name),
-            attr.c.aobs.label(obs_column_name),
-        ).where(attr.c.the_type.like(the_type))
-        cols = ["id", "name", "sex", column_name, date_column_name, obs_column_name]
-    else:  # no person information required we use the attributes table
-        attr = db.get_nattribute_table()
-        id_col = attr.c.entity
-        stmt = select(
-            attr.c.entity,
+    # entity_model = db.get_model(entity_type)
+    # get the columns of the entity table, check if more_info is valid
+    entity_table = db.get_table(entity_type)
+    entity_columns = entity_table.columns.keys()
+    if more_info is None:
+        more_info = []
+
+    extra_cols = []
+
+    # collect the column objects for the select list
+    for mi in more_info:
+        if mi not in entity_columns:
+            raise ValueError(f"{mi} is not a valid column for {entity_type}")
+        else:
+            extra_cols.append(entity_table.columns[mi])
+
+    if name_like is not None:
+        if "name" not in entity_columns:
+            raise (ValueError("To filter by name requires person_info=True."))
+
+    attr = db.get_table("attribute")
+    id_col = attr.c.entity.label("id")
+    cols = [id_col]
+    cols.extend(extra_cols)
+    cols.extend(
+        [
             attr.c.the_value.label(column_name),
             attr.c.the_date.label(date_column_name),
             attr.c.obs.label(obs_column_name),
-        ).where(attr.c.the_type.like(the_type))
-        cols = ["id", column_name, date_column_name, obs_column_name]
+        ]
+    )
+
+    stmt = (
+        select(*cols)
+        .where(attr.c.the_type.like(the_type))
+        .join(entity_table, entity_table.c.id == attr.c.entity)
+    )
 
     # Filter by value
     if the_value is not None:
@@ -106,7 +122,7 @@ def entities_with_attribute(
 
     # filter by name
     if name_like is not None:
-        stmt = stmt.where(attr.c.name.like(name_like))
+        stmt = stmt.where(entity_table.columns["name"].like(name_like))
 
     stmt = stmt.order_by(attr.c.the_date)
 
@@ -115,7 +131,8 @@ def entities_with_attribute(
 
     with db.session() as session:
         records = session.execute(stmt)
-        df = pd.DataFrame.from_records(records, index=["id"], columns=cols)
+        col_names = stmt.columns.keys()
+        df = pd.DataFrame.from_records(records, index=["id"], columns=col_names)
 
     if df.iloc[0].count() == 0:
         return None  # nothing found we
@@ -126,25 +143,26 @@ def entities_with_attribute(
         more_columns = more_cols
 
     if len(more_columns) > 0:
-        attr = db.get_nattribute_view()
-        id_col = attr.c.id
+        id_col = attr.c.entity
 
         for mcol in more_columns:
             column_name = mcol
             date_column_name = f"{column_name}.date"
             obs_column_name = f"{column_name}.obs"
             stmt = select(
-                attr.c.id,
+                attr.c.entity.label("id"),
                 attr.c.the_value.label(column_name),
                 attr.c.the_date.label(date_column_name),
-                attr.c.aobs.label(obs_column_name),
+                attr.c.obs.label(obs_column_name),
             ).where(attr.c.the_type == mcol)
             stmt = stmt.where(id_col.in_(df.index))
-            cols = ["id", column_name, date_column_name, obs_column_name]
+            col_names = stmt.columns.keys()
 
             with db.session() as session:
                 records = session.execute(stmt)
-                df2 = pd.DataFrame.from_records(records, index=["id"], columns=cols)
+                df2 = pd.DataFrame.from_records(
+                    records, index=["id"], columns=col_names
+                )
 
             if sql_echo:
                 print(f"Query for more_columns={mcol}:\n", stmt)
