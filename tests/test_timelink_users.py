a0064276335b60f5cf54cab32eed2e83570a991f
Joaquim Carvalho <joaquim@uc.pt>
2024-02-19 12:58:40 +0800

    Fix user database tests on Travis

--- tests/test_timelink_users.py
+++ tests/test_timelink_users.py
@@ -1,3 +1,4 @@
+import os
 import time
 from timelink.api import database
 from timelink.app.models.user import User, UserProperty  # noqa
@@ -9,9 +10,11 @@ import random
 import string
 from sqlalchemy.exc import IntegrityError
 
+from tests import skip_on_travis
+
 
 @pytest.fixture(scope="module")
-def dbsystem(request):
+def dbsystem(request: pytest.FixtureRequest):
     """Create a database for testing"""
     db_type, db_name, db_path = request.param
     db_user = None
@@ -22,7 +25,11 @@ def dbsystem(request):
         db_pwd = database.get_postgres_container_pwd()
 
     dbsystem = UserDatabase(
-        db_name=db_name, db_type=db_type, db_user=db_user, db_pwd=db_pwd
+        db_name=db_name,
+        db_type=db_type,
+        db_user=db_user,
+        db_pwd=db_pwd,
+        db_path=db_path
     )
 
     with dbsystem.session() as session:
@@ -34,21 +41,30 @@ def dbsystem(request):
     try:
         yield dbsystem
     finally:
-        # dbsystem.drop_db
+        dbsystem.drop_db
         dbsystem.session().close()
 
 
-@pytest.mark.parametrize(
+dbparam = pytest.mark.parametrize(
     "dbsystem",
     [
         # db_type, db_name, db_path
         ("sqlite", ":memory:", None),
+        # see https://doc.pytest.org/en/latest/how-to/skipping.html#skip-xfail-with-parametrize
+        # Not working
+        # (pytest.param("postgres", "tests_users", None)),
         ("postgres", "tests_users", None),
     ],
     indirect=True,
 )
+
+
+@dbparam
 def test_create_user_with_properties(dbsystem):
     db = dbsystem
+    if db.db_type == "postgres":
+        if os.environ.get('TRAVIS') == 'true':
+            pytest.xfail("No postgres on Travis")
     with db.session() as session:
         user = User(
             name="User One",
@@ -91,17 +107,12 @@ def test_create_user_with_properties(dbsystem):
         assert user.updated > user.created
 
 
-@pytest.mark.parametrize(
-    "dbsystem",
-    [
-        # db_type, db_name, db_path
-        ("sqlite", ":memory:", None),
-        ("postgres", "tests_users", None),
-    ],
-    indirect=True,
-)
+@dbparam
 def test_create_user_db(dbsystem):
     db: UserDatabase = dbsystem
+    if db.db_type == "postgres":
+        if os.environ.get('TRAVIS') == 'true':
+            pytest.xfail("No postgres on Travis")
     with db:
         user = User(
             name="User 2",
@@ -115,98 +126,80 @@ def test_create_user_db(dbsystem):
         assert user2 is not None
 
 
-@pytest.mark.parametrize(
-    "dbsystem",
-    [
-        # db_type, db_name, db_path
-        ("sqlite", ":memory:", None),
-        ("postgres", "tests_users", None),
-    ],
-    indirect=True,
-)
+@dbparam
 def test_set_user_property(dbsystem):
     db: UserDatabase = dbsystem
-    db.start_session()
-    user = User(
-        name="User 3",
-        fullname="Tertius",
-        email="san@xpto.org",
-        nickname="三",
-    )
-    db.add_user(user)
-    db.commit()  # otherwise user.id is None
-    db.set_user_property(user.id, "property1", "value1")
-    db.set_user_property(user.id, "property2", "value2")
-    db.commit()
-    user = db.get_user_by_email("san@xpto.org")
-    assert user is not None
-    assert len(user.properties) == 2
-    props = db.get_user_properties(user.id)
-    assert len(props) == 2
-    proval = db.get_user_property(user.id, "property1")
-    assert proval.value == "value1"
-    proval = db.get_user_property(user.id, "property2")
-    assert proval.value == "value2"
-    db.close_session()
+    if db.db_type == "postgres":
+        if os.environ.get('TRAVIS') == 'true':
+            pytest.xfail("No postgres on Travis")
+    with db:  # noqa
+        user = User(
+            name="User 3",
+            fullname="Tertius",
+            email="san@xpto.org",
+            nickname="三",
+        )
+        db.add_user(user)
+        db.commit()  # otherwise user.id is None
+        db.set_user_property(user.id, "property1", "value1")
+        db.set_user_property(user.id, "property2", "value2")
+        db.commit()
+        user = db.get_user_by_email("san@xpto.org")
+        assert user is not None
+        assert len(user.properties) == 2
+        props = db.get_user_properties(user.id)
+        assert len(props) == 2
+        proval = db.get_user_property(user.id, "property1")
+        assert proval.value == "value1"
+        proval = db.get_user_property(user.id, "property2")
+        assert proval.value == "value2"
 
 
-@pytest.mark.parametrize(
-    "dbsystem",
-    [
-        # db_type, db_name, db_path
-        ("sqlite", ":memory:", None),
-        ("postgres", "tests_users", None),
-    ],
-    indirect=True,
-)
+@dbparam
 def test_update_user(dbsystem):
     db: UserDatabase = dbsystem
-    db.start_session()
-    user = User(
-        name="User4",
-        fullname="Utilizador Quatro",
-        email="four@xpto.com",
-        nickname="Test Nickname",
-    )
-    db.add_user(user)
-    db.commit()
-    user = db.get_user_by_email("four@xpto.com")
-    assert user.fullname == "Utilizador Quatro"
-    user.fullname = "Quarto Utilizador"
-    db.update_user(user)
-    db.commit()
-    user = db.get_user_by_email("four@xpto.com")
-    assert user.fullname == "Quarto Utilizador"
+    if db.db_type == "postgres":
+        if os.environ.get('TRAVIS') == 'true':
+            pytest.xfail("No postgres on Travis")
+    with db:
+        user = User(
+            name="User4",
+            fullname="Utilizador Quatro",
+            email="four@xpto.com",
+            nickname="Test Nickname",
+        )
+        db.add_user(user)
+        db.commit()
+        user = db.get_user_by_email("four@xpto.com")
+        assert user.fullname == "Utilizador Quatro"
+        user.fullname = "Quarto Utilizador"
+        db.update_user(user)
+        db.commit()
+        user = db.get_user_by_email("four@xpto.com")
+        assert user.fullname == "Quarto Utilizador"
 
 
-@pytest.mark.parametrize(
-    "dbsystem",
-    [
-        # db_type, db_name, db_path
-        ("sqlite", ":memory:", None),
-        ("postgres", "tests_users", None),
-    ],
-    indirect=True,
-)
+@dbparam
 def test_duplicate_email(dbsystem):
     db: UserDatabase = dbsystem
-    db.start_session()
-
+    if db.db_type == "postgres":
+        if os.environ.get('TRAVIS') == 'true':
+            pytest.xfail("No postgres on Travis")
     with db.session() as session:
         user = User(
-            name="User One",
-            fullname="Tests User One",
-            email="one@xpto.com",
-            nickname="One",
+            name="User five",
+            fullname="Tests User five",
+            email="five@xpto.com",
+            nickname="five",
         )
         session.add(user)
         session.commit()
 
         duplicate_user = User(
-            name="User Two",
-            fullname="Tests User Two",
-            email="one@xpto.com",
-            nickname="Two",
+            name="User six",
+            fullname="Tests User six",
+            email="five@xpto.com",
+            nickname="six",
         )
 
         with pytest.raises(IntegrityError):  # Replace Exception with a more specific exception
