12a12c09a77a60fe92c47bfa353e6c79cb41b29a
Joaquim Carvalho <joaquim@uc.pt>
2024-02-26 15:21:18 +0800

    Improve logging.

--- tests/__init__.py
+++ tests/__init__.py
@@ -4,6 +4,7 @@ import os
 from pathlib import Path
 import random
 import time
+import warnings
 
 from sqlalchemy.orm import sessionmaker
 import pytest
@@ -39,13 +40,13 @@ skip_on_travis = pytest.mark.skipif(
 
 def get_one_translation(kserver: KleioServer,
                         path="",
-                        max_wait=360) -> KleioFile:
+                        max_wait=120) -> KleioFile:
     """Get one translation from the server"""
     translations = kserver.translation_status(path=path, recurse="yes", status="V")
     if len(translations) == 0:
-        need_translation = kserver.translation_status(path="", recurse="yes", status="T")
+        need_translation = kserver.translation_status(path=path, recurse="yes", status="T")
         if len(need_translation) == 0:
-            raise RuntimeError("No files available for translation")
+            raise RuntimeError(f"No files available for translation, path={path}")
 
         one_translation = random.choice(need_translation)
         kserver.translate(path=one_translation.path, recurse="no")
@@ -53,12 +54,17 @@ def get_one_translation(kserver: KleioServer,
         counter = 0
         wait_for = 10
         max_counter = max_wait / wait_for
+        translations = kserver.translation_status(path=path, recurse="yes", status="V")
         in_process = kserver.translation_status(path=path, recurse="yes", status="P")
         while len(translations) == 0 and (counter < max_counter
                                           or len(in_process) > 0):  # noqa: W503
             time.sleep(wait_for)
             counter += 1
-            translations = kserver.translation_status(path="", recurse="yes", status="V")
+            if counter % 20 == 0:
+                warnings.warn(f"Waiting for translations {one_translation.name}, counter={counter}",
+                              stacklevel=1)
+            translations = kserver.translation_status(path=path, recurse="yes", status="V")
+            in_process = kserver.translation_status(path=path, recurse="yes", status="P")
 
     if len(translations) > 0:
         kleio_file: KleioFile = random.choice(translations)
