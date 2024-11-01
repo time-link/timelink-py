9e11a0c4111a801f2d57f3e71da20a9a220467e1
Joaquim Carvalho (MPU) <joaquimcarvalho@mpu.edu.mo>
2024-11-01 18:00:39 +0800

    Add migrations to cli interface

--- timelink/cli.py
+++ timelink/cli.py
@@ -5,19 +5,35 @@ Also provides basic mhk manager functionality.
 Run with  python -m timelink.cli
 
 """
-
+import os
+from typing import Annotated
 import platform
 import uvicorn
 import typer
 import docker
 from timelink.mhk.utilities import get_mhk_info, is_mhk_installed
+from timelink import migrations
+from timelink.api.database import get_postgres_dbnames
+from timelink.api.database import get_sqlite_databases
+from timelink.api.database import get_postgres_url
+from timelink.api.database import get_sqlite_url
+from pathlib import Path
+
 
 server: uvicorn.Server = None  # uvicorn server instance
+
+# get the current directory
+# this is used to find the alembic.ini file
+# and the migrations folder
+# ROOT_PATH = Path(__file__).parent.parent
+current_working_directory = os.getcwd()
 
 # We use Typer https://typer.tiangolo.com
 app = typer.Typer(help="Timelink and MHK manager")
 mhk_app = typer.Typer()
 app.add_typer(mhk_app, name="mhk", help="MHK legacy manager")
+db_app = typer.Typer()
+app.add_typer(db_app, name="db", help="Database manager")
 
 
 # Start server
@@ -25,6 +41,9 @@ app.add_typer(mhk_app, name="mhk", help="MHK legacy manager")
 # https://github.com/miguelgrinberg/python-socketio/issues/332#issuecomment-712928157
 # https://stackoverflow.com/questions/68603658/how-to-terminate-a-uvicorn-fastapi-application-cleanly-with-workers-2-when
 
+def cli_header():
+    typer.echo("Timelink and MHK manager")
+    typer.echo(f"{current_working_directory}")
 
 @app.command("start")
 def start():
@@ -32,12 +51,43 @@ def start():
     typer.echo("Starting Timelink")
     config = uvicorn.Config("timelink.app.main:app", port=8008, reload=True)
 
-    global server
+    global server  # pylint: disable=global-statement
     server = uvicorn.Server(config)
     server.run()
     return 0
 
+# DB related commands
+# These are used to manage the database migrations
 
+
+@db_app.command("list")
+def db_database_list_cmd():
+    """List all available databases (sqlite, postgresql, etc)"""
+    postgres_list = get_postgres_dbnames()
+    sqlite_list = get_sqlite_databases(current_working_directory)
+    if postgres_list:
+        typer.echo("PostgreSQL databases:")
+        for db in postgres_list:
+            typer.echo(f"    {db} ({get_postgres_url(db)})")
+    if sqlite_list:
+        typer.echo("SQLite databases:")
+        for db in sqlite_list:
+            # extract the database name from the path
+            db_name = db.split("/")[-1]
+            typer.echo(f"    {db_name} ({get_sqlite_url(db)})")
+
+
+@db_app.command("current")
+def db_current_cmd(
+    db_url: str,
+    verbose: str = "--verbose",
+):
+    """Display current database revision"""
+    pass
+    migrations.current(db_url, verbose)
+
+
+# MHK related commands
 @mhk_app.command(name="version")
 def mhk_version():
     """shows MHK manager version
@@ -48,26 +98,26 @@ def mhk_version():
     if is_mhk_installed():
         mhk_info = get_mhk_info()
 
-    try:
-        client = docker.from_env(version="auto")
-        dv = client.version()
-        mhkv = f"""    Manager version:  {mhk_info.mhk_version}
-        Docker version:   {dv["Version"]}
-        Host OS:          {platform.system()} {platform.release()}
-        User home:        {mhk_info.user_home}
-        mhk-home:         {mhk_info.mhk_home}
-        mhk-home init:    {mhk_info.mhk_home_init}
-        mhk-home update:  {mhk_info.mhk_home_update}
-        mhk use-tag:      {mhk_info.mhk_app_env.get("TAG", "*none*")}
-        mhk local host:   {mhk_info.mhk_host}
-        MHK URL:          http://127.0.0.1:8080/mhk
-        Kleio URL:        http://127.0.0.1:8088
-        Portainer URL:    http://127.0.0.1:9000"""
-        typer.echo(mhkv)
-    except Exception as e:
-        typer.echo(f"Could not access docker: {e}")
-    else:
-        type.echo("Could not find a MHK instalation")
+        try:
+            client = docker.from_env(version="auto")
+            dv = client.version()
+            mhkv = f"""    Manager version:  {mhk_info.mhk_version}
+            Docker version:   {dv["Version"]}
+            Host OS:          {platform.system()} {platform.release()}
+            User home:        {mhk_info.user_home}
+            mhk-home:         {mhk_info.mhk_home}
+            mhk-home init:    {mhk_info.mhk_home_init}
+            mhk-home update:  {mhk_info.mhk_home_update}
+            mhk use-tag:      {mhk_info.mhk_app_env.get("TAG", "*none*")}
+            mhk local host:   {mhk_info.mhk_host}
+            MHK URL:          http://127.0.0.1:8080/mhk
+            Kleio URL:        http://127.0.0.1:8088
+            Portainer URL:    http://127.0.0.1:9000"""
+            typer.echo(mhkv)
+        except Exception as e:
+            typer.echo(f"Could not access docker: {e}")
+        else:
+            type.echo("Could not find a MHK instalation")
     return 0
 
 
