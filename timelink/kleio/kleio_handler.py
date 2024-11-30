730d0a48d89713e71a75b51b179821329cdd00f3
Joaquim Carvalho <joaquim@uc.pt>
2024-11-30 12:12:08 +0800

    Handling of source when reimporting

--- timelink/kleio/kleio_handler.py
+++ timelink/kleio/kleio_handler.py
@@ -4,7 +4,7 @@ from enum import Enum
 from typing import List
 import copy
 
-from sqlalchemy import delete
+from sqlalchemy import text, delete
 from sqlalchemy.orm import Session
 from sqlalchemy.exc import IntegrityError
 
@@ -15,8 +15,17 @@ from timelink.api.models.pom_som_mapper import (
 from timelink.api.models.base_mappings import (
     pom_som_base_mappings as pom_som_base_mappingsTL,
 )
-from timelink.api.models.base import Entity as EntityTL, Person as PersonTL
-from timelink.api.models.base import REntity, REntityStatus as STATUS
+from timelink.api.models.base import (
+    Entity as EntityTL,
+    Person as PersonTL,
+    Relation as RelationTL
+)
+from timelink.api.models.rentity import (
+    REntity as REntityTL,
+    Link as LinkTL
+)
+
+from timelink.api.models.base import REntity, LinkStatus as STATUS
 from timelink.api.models.system import KleioImportedFile as KleioFileTL
 
 
@@ -27,6 +36,7 @@ from timelink.mhk.models.pom_som_mapper import (
 )
 from timelink.mhk.models.entity import Entity as EntityMHK
 from timelink.mhk.models.person import Person as PersonMHK
+from timelink.mhk.models.relation import Relation as RelationMHK
 from timelink.mhk.models.system import KleioImportedFile as KleioFileMHK
 
 from timelink.kleio.groups import KGroup
@@ -46,7 +56,7 @@ class KleioContext(Enum):
 
 
 class KleioHandler:
-    """ This class handles the import of data from a Kleio XML file into a Timelink database.
+    """This class handles the import of data from a Kleio XML file into a Timelink database.
 
     It implements an interface to the SAX parser that reads the XML file and
     generates Kleio events. The KleioHandler class processes these events and
@@ -57,7 +67,8 @@ class KleioHandler:
         newClass:  definition of a new class (mapping between SOM and POM)
         newGroup:  a new kleio Group
         newRelation:  a new meta relation between kleio groups such as same_as
-        """
+    """
+
     pom_som_base_mappings = None
     pom_som_mapper = None
     pom_class_attributes = None
@@ -66,6 +77,9 @@ class KleioHandler:
     kleio_file_model = None
     model_type: str = None
     postponed_relations = []
+    sources_in_file = []
+    xrelations = {}
+    xlinks = {}
     errors = []
     warnings = []
     kleio_file_origin = None
@@ -94,6 +108,9 @@ class KleioHandler:
             self.pom_class_attributes = PomClassAttributesTL
             self.entity_model = EntityTL
             self.person_model = PersonTL
+            self.relation_model = RelationTL
+            self.rentity_model = REntityTL
+            self.link_model = LinkTL
             self.kleio_file_model = KleioFileTL
             self.model_type = "TL"
         elif mode == "MHK":
@@ -102,10 +119,184 @@ class KleioHandler:
             self.pom_class_attributes = PomClassAttributesMHK
             self.entity_model = EntityMHK
             self.person_model = PersonMHK
+            self.relation_model = RelationMHK
             self.kleio_file_model = KleioFileMHK
             self.model_type = "MHK"
         else:
             raise ValueError(f"Unknown mode {mode}")
+
+    def save_source_context(self, source_id):
+        """Save all the links and relations pointing to this
+        source from other sources because they will be nulled or deleted
+        when the source is deleted.
+
+        After import we will restore them.
+
+        This will add to the self.cross_source_references dict
+
+        We use the follow queries to get the external relations to this source:
+
+        ```sql
+        SELECT r.id, r.origin, r.destination, r.the_type, r.the_value
+                FROM relations r, entities e
+                WHERE r.id = e.id
+                            AND e.the_source != :source_id
+                            AND r.destination IN
+                                (SELECT id
+                                 FROM entities e2
+                                 WHERE e2.id = r.destination
+                                                AND e2.the_source != e.the_source)
+
+        ```
+        And to get the external links to this source:
+
+        ```sql
+        SELECT l.rid,l.entity,l.source as link_source, e.the_source as entity_source
+                FROM links l, entities e
+                WHERE l.entity = e.id
+                AND e.the_source = :source_id
+                AND link_source != e.the_source;
+        """
+        # make a sql alchemy query to get the cross source relations
+        # and store them in cross_source_relations
+        sql = """select r.id,
+                    r.origin,
+                    r.destination,
+                    r.the_type,
+                    r.the_value,
+                    r.the_date,
+                    e.the_source,
+                    e2.the_source as dest_source
+                        FROM relations r, entities e, entities e2
+                        WHERE r.id = e.id
+                            AND r.destination = e2.id
+                            AND e.the_source != :source_id
+                            AND e.the_source != e2.the_source
+                """
+        # https://docs.sqlalchemy.org/en/20/orm/queryguide/select.html#orm-queryguide-selecting-text
+        sql_query = (
+            text(sql)
+            .bindparams(source_id=source_id)
+            .columns(
+                self.relation_model.id,
+                self.relation_model.origin,
+                self.relation_model.destination,
+                self.relation_model.the_type,
+                self.relation_model.the_value,
+                self.relation_model.the_date,
+                self.entity_model.the_source,
+                self.entity_model.the_source.label("dest_source"),
+            )
+        )
+        xrefs = self.session.execute(sql_query).fetchall()
+
+        self.xrelations[source_id] = xrefs
+
+        # get the links that are going to be affected by the reimport
+        sql = """
+                SELECT l.id, l.rid,l.entity, l.user, l.rule, l.status, l.source as link_source, e.the_source as entity_source
+                FROM links l, entities e
+                WHERE l.entity = e.id
+                AND e.the_source = :source_id;
+
+        """
+        sql_query = (
+            text(sql)
+            .bindparams(source_id=source_id)
+            .columns(
+                self.link_model.id,
+                self.link_model.rid,
+                self.link_model.entity,
+                self.link_model.user,
+                self.link_model.rule,
+                self.link_model.status,
+                self.link_model.source,
+                self.entity_model.the_source.label('entity_source')
+            )
+        )
+        xlinks = self.session.execute(sql_query).fetchall()
+        for link_id in [link.id for link in xlinks]:
+            link = self.session.get(self.link_model, link_id)
+            link.status = STATUS.INVALID
+            self.session.merge(link)
+            self.session.commit()
+
+        self.xlinks[source_id] = xlinks
+
+    def restore_source_context(self, source_id):
+        """Restore all the links and relations pointing to this
+        source from other sources that were saved before the source was deleted
+
+        TODO: is this better here or in Source entity model?
+        Reasons for here: at the import level restore only
+        makes sense after all the source is imported because
+        at the entity level a Source is inserted empty and only
+        at the end of the source import it is possible to restore
+        the missing links
+        """
+
+        relations = self.xrelations[source_id]
+        for r in relations:
+            # we get the relation in other sources that was affect by the reimport
+            rel = self.session.get(
+                self.relation_model, r.id
+            )  # safe because id is on the exernal source
+            if rel is not None:
+                # this relation should have the destination None
+                # because the destination was deleted when the current source
+                # was deleted before reimport
+                # check that the old destination still exists after reimport
+                if rel.destination is not None:
+                    # if the relation has a destination it means that it was not affected
+                    # by the reimport of the source, but it should
+                    # show a warning
+                    self.warnings.append(
+                        f"WARNING: An external relation {r.id} "
+                        f"of type {r.the_type} with value {r.the_value} "
+                        f"referred entity {r.destination} in source {r.dest_source} "
+                        f"was not affected by reimport of source {source_id}"
+                    )
+                dest = self.session.get(self.entity_model, r.destination)
+                if dest is not None:
+                    # if so we restore the relation destination
+                    rel.destination = r.destination  # set to the saved value
+                    self.session.merge(rel)
+                    self.session.commit()
+
+                    # TODO: and now we should process the xsame_as relations
+                    if rel.the_type == 'identification' and rel.the_value == 'same as':
+
+                        REntity.same_as(
+                            rel.origin,
+                            rel.destination,
+                            status=STATUS.SOURCE,
+                            rule=f"same_as('{self.kleio_file_name}', 'reimported')",
+                            source=source_id,
+                            session=self.session,
+                        )
+                        pass
+                else:
+                    # if the destination does not exist
+                    # we flag the error
+                    self.errors.append(
+                        f"ERRROR: An external relation {r.id} "
+                        f"of type {r.the_type} with value {r.the_value} "
+                        f"referred entity {r.destination} which does not exist "
+                        f"after reimport of source {source_id}"
+                    )
+                self.session.commit()
+            else:
+                # The saved relation in the other source was deleted
+                # this should not have happened
+                self.errors.append(
+                    f"ERRROR: An external relation {r.id} "
+                    f"of type {r.the_type} with value {r.the_value} "
+                    f"with origin {r.origin} and destination {r.destination}"
+                    f"was saved before reimport of source {source_id} "
+                    f"but was deleted during reimport."
+                )
+
+        return relations
 
     def newKleioFile(self, attrs):
         # <KLEIO STRUCTURE="/kleio-home/system/conf/kleio/stru/gacto2.str"
@@ -132,6 +323,9 @@ class KleioHandler:
         self.kleio_source_id = None
         self.aregister_id = None
         self.pom_som_cache = dict()
+        self.sources_in_file = []
+        self.xrefs = {}
+        self.xlinks = {}
         logging.debug("Kleio file: %s", self.kleio_file)
         logging.debug("Kleio structure: %s", self.kleio_structure)
         logging.debug("Kleio translator: %s", self.kleio_translator)
@@ -217,7 +411,9 @@ class KleioHandler:
                 raise ValueError(
                     f"Could not find PomSomMapper for class {group.pom_class_id}"
                 )
-            self.pom_som_cache[group.pom_class_id] = pom_mapper_for_group  # TODO redundant?
+            self.pom_som_cache[group.pom_class_id] = (
+                pom_mapper_for_group  # TODO redundant?
+            )
         except Exception as exc:
             self.errors.append(
                 f"ERROR: {self.kleio_file_name} {str(group.line)}"
@@ -236,13 +432,22 @@ class KleioHandler:
         if group.pom_class_id == "act":  # TODO should be .extends("act")
             logging.debug(f"{group._name}${group.id}")
 
-        if pom_mapper_for_group.id == "aregister":  # TODO should be .extends("aregister")
+        if (
+            pom_mapper_for_group.id == "aregister"
+        ):  # TODO should be .extends("aregister")
             self.kleio_file_is_aregister = True  # current file is an authority register
             self.aregister_id = str(group.id.core)
 
         if pom_mapper_for_group.id == "source":  # TODO should be .extends("source")
             self.kleio_source_id = str(group.id.core)
             group.source_id = self.kleio_source_id
+            self.sources_in_file.append(self.kleio_source_id)
+            # We need to save all the links and relations pointing to this
+            # source from other sources because they will be nulled or deleted
+            # when the source is deleted
+            # after import we will restore them for the entities that are restored
+            # in this import
+            self.save_source_context(self.kleio_source_id)
 
         if pom_mapper_for_group.id == "relation":  # TODO should be .extends("relation")
             # it can happen that the destination of a relation
@@ -250,7 +455,8 @@ class KleioHandler:
             #  In this case we postpone the storing of the relation
             # until the end of file
             exits_dest_rel = self.session.get(
-                self.entity_model, group.get_element_by_name_or_class("destination").core
+                self.entity_model,
+                group.get_element_by_name_or_class("destination").core,
             )
             if exits_dest_rel is None:
                 self.postponed_relations.append(
@@ -286,7 +492,7 @@ class KleioHandler:
                 )
 
     def newRelation(self, attrs):
-        """ Process a new (Meta) relation
+        """Process a new (Meta) relation
 
         Currently there are two types of metadata relations,
         both related to processing same as relations:
@@ -326,12 +532,14 @@ class KleioHandler:
             # register the same_as relation
 
             try:
-                REntity.same_as(rel_origin,
-                                rel_dest,
-                                status=STATUS.SOURCE,
-                                rule=f"same_as('{self.kleio_file_name}')",
-                                source=self.kleio_source_id,
-                                session=self.session)
+                REntity.same_as(
+                    rel_origin,
+                    rel_dest,
+                    status=STATUS.SOURCE,
+                    rule=f"same_as('{self.kleio_file_name}')",
+                    source=self.kleio_source_id,
+                    session=self.session,
+                )
                 self.session.commit()
             except Exception as exc:
                 self.errors.append(
@@ -352,9 +560,7 @@ class KleioHandler:
                     f"Real entity {rel_dest} not found"
                 )
                 logging.error(error_msg)
-                self.errors.append(
-                    error_msg
-                )
+                self.errors.append(error_msg)
                 self.session.rollback()
                 return
             try:
@@ -372,16 +578,13 @@ class KleioHandler:
                     f"processing attach_to_rentity relation {rel_id}: {exc.__class__.__name__}: {exc}"
                 )
                 logging.error(msg)
-                self.errors.append(
-                    msg
-                )
+                self.errors.append(msg)
                 self.session.rollback()
                 return
 
         else:  # unknown relation
             self.errors.append(
-                f"ERROR: {self.kleio_file_name} "
-                f"unknown relation type {rel_value}"
+                f"ERROR: {self.kleio_file_name} " f"unknown relation type {rel_value}"
             )
             self.session.rollback()
 
@@ -410,6 +613,14 @@ class KleioHandler:
                 )
                 self.session.rollback()
         self.postponed_relations = []
+
+        # restore all the links and relations pointing to this
+        # source(s) from other sources that were saved before the source was deleted
+        # the cross references were save when a source group was processed
+        # TODO: should this go here or to models.Source?
+        for source_id in self.sources_in_file:
+            self.restore_source_context(source_id)
+
         # store information on kleio file import
         kfile = self.kleio_file_model(
             path=self.kleio_file,  # TODO: #20 should remove /Kleio-home from path
