a821106fd6e502587b1b275fa4a4d6523f69e9ab
Joaquim Carvalho <joaquim@uc.pt>
2024-12-06 11:26:15 +0800

    Add naming conventions to models metadata

--- timelink/api/models/base_class.py
+++ timelink/api/models/base_class.py
@@ -6,12 +6,25 @@
 """
 
 from typing import Any
+from sqlalchemy import MetaData  # pylint: disable=import-error
 from sqlalchemy.orm import DeclarativeBase  # pylint: disable=import-error
 from sqlalchemy.orm import declared_attr  # pylint: disable=import-error
 
 
+naming_convention = {
+    "ix": "ix_%(column_0_label)s",
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_`%(constraint_name)s`",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s",
+}
+
+
 class Base(DeclarativeBase):
     """Base class, all modules must import this to share Metadata"""
+
+    # see https://alembic.sqlalchemy.org/en/latest/naming.html#naming-conventions
+    metadata = MetaData(naming_convention=naming_convention)
 
     # see https://docs.sqlalchemy.org/en/20/changelog/migration_20.html#migration-20-step-six
     __allow_unmapped__: bool = True
