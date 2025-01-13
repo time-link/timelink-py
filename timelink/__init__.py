2a38547f73458c6c3ae4feb590647961500c13fd
Joaquim Carvalho <joaquim@uc.pt>
2025-01-13 03:49:12 +0000

    Improve get_latest_version of package

--- timelink/__init__.py
+++ timelink/__init__.py
@@ -25,10 +25,8 @@ __version__ = '1.1.15'
 version = __version__
 
 
-def get_latest_version(package_name):
+def get_latest_version(package_name="timelink"):
     """Get the latest version of a package from PyPI."""
-    if not package_name:
-        package_name = 'timelink'
     # Step 1: Fetch the package information from PyPI
     url = f'https://pypi.org/pypi/{package_name}/json'
     response = requests.get(url)
