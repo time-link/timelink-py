6b54cca70b3843f6e5463f7f6862e5ecafa85980
Joaquim Carvalho <joaquim@uc.pt>
2026-01-31 21:08:04 +0800

    Bump version: 1.1.30 → 1.1.31

--- timelink/__init__.py
+++ timelink/__init__.py
@@ -20,7 +20,7 @@ from .api import schemas  # noqa: F401
 
 __author__ = """Joaquim Ramos de Carvalho"""
 __email__ = 'joaquimcarvalho@mpu.edu.mo'
-__version__ = '1.1.30'
+__version__ = '1.1.31'
 
 version = __version__
 
